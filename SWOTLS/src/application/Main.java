157a238ae89e9a950991654d4abd5f937f3faef2
Kubpica <kubpicapf@gmail.com>
2018-11-19 22:18:57 +0100

    Hello GUI

--- SWOTLS/src/application/Main.java
+++ SWOTLS/src/application/Main.java
@@ -1,19 +1,19 @@
 package application;
-	
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+			primaryStage.setTitle("Hello World");
+			primaryStage.setScene(new Scene(root, 300, 275));
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
