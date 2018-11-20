9adcc283e17afa245dcf3bc025908ad14e5bd3b1
Kubpica <kubpicapf@gmail.com>
2018-11-20 03:13:12 +0100

    GUI - Switching scenes

--- SWOTLS/src/application/Main.java
+++ SWOTLS/src/application/Main.java
@@ -2,25 +2,75 @@ package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 
+/**
+ * Main application class.
+ */
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-			primaryStage.setTitle("Hello World");
-			primaryStage.setScene(new Scene(root, 300, 275));
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        stage.setTitle("SWOTLS");
+
+        stage.setScene(
+            createScene(
+                loadMainPane()
+            )
+        );
+
+        stage.show();
+    }
+
+    /**
+     * Loads the main fxml layout.
+     * Sets up the vista switching VistaNavigator.
+     * Loads the first vista into the fxml layout.
+     *
+     * @return the loaded pane.
+     * @throws IOException if the pane could not be loaded.
+     */
+    private Pane loadMainPane() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+
+        Pane mainPane = (Pane) loader.load(
+            getClass().getResourceAsStream(
+                VistaNavigator.MAIN
+            )
+        );
+
+        MainController mainController = loader.getController();
+
+        VistaNavigator.setMainController(mainController);
+        VistaNavigator.loadVista(VistaNavigator.VISTA_1);
+
+        return mainPane;
+    }
+
+    /**
+     * Creates the main application scene.
+     *
+     * @param mainPane the main application layout.
+     *
+     * @return the created scene.
+     */
+    private Scene createScene(Pane mainPane) {
+        Scene scene = new Scene(
+            mainPane
+        );
+
+        scene.getStylesheets().setAll(
+            getClass().getResource("/application/vista.css").toExternalForm()
+        );
+
+        return scene;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
